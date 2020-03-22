5a9201d19223171e90becaa8fa39341300f2912a
yangzeyu1026 <yangzeyu1026@sjtu.edu.cn>
2020-03-22 22:17:41 +0800

    no message

---
+++ 040401.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++>
+
+void guess(char ch)
+{
+	int bad = 1;
+	for (int i = 0; i < strlen(s); i++)
+		if(s[i]==ch) {left--;s[i] = ' ';bad = 0;}
+	if(bad) --chance;
+	if(!chance) lose = 1;
+	if(!left) win = 1;
+}
